197e44265e2fba5d1b96cabfb8c07313062f9317
Basit46 <Hassanbasit1990@gmail.com>
2024-02-03 11:59:24 +0100

    Added hero image transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Booking from "./sections/Booking";
@@ -8,11 +8,43 @@ import Discover from "./sections/Discover";
 import Features from "./sections/Features";
 import PreFooter from "./sections/PreFooter";
 import Footer from "./sections/Footer";
+import heroBg1 from "./assets/hero1.svg";
+import heroBg2 from "./assets/hero2.jpg";
+import heroBg3 from "./assets/hero3.jpg";
+import heroBg4 from "./assets/hero4.jpg";
+import heroBg5 from "./assets/hero5.jpg";
 
 const App = () => {
+  const images = [heroBg1, heroBg2, heroBg3, heroBg4, heroBg5];
+  const [currId, setCurrId] = useState(0);
+
+  useEffect(() => {
+    var count = 1;
+    const interval = setInterval(() => {
+      if (count < images.length) {
+        setCurrId(count);
+        count++;
+      } else {
+        setCurrId(0);
+        count = 1;
+      }
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div>
-      <div className="relative heroBg min-h-[100vh] text-white">
+      <div className="relative min-h-[100vh] text-white">
+        <div className="z-[-1] absolute h-full w-full bg-[skyblue]">
+          <img
+            className="h-full w-full object-cover"
+            src={images[currId]}
+            alt="vacation"
+          />
+        </div>
         <Navbar />
         <Hero />
         <Booking />
